2da3d19e94f818a46e0b0f66106fdbf0469037df
rykersun <rykersun2002@gmail.com>
2022-07-08 22:21:14 +0800

    complete add_node.c && remove test_memory.c

--- LinkedList/add_node.c
+++ LinkedList/add_node.c
@@ -1,40 +1,59 @@
 #include <stdio.h>
-
-// malloc required
 #include <stdlib.h>
 
-// define node
 struct node
 {
     int data;
     struct node *next;
 };
 
-int main(void)
+typedef struct node node_t;
+
+void print_list(node_t *head)
 {
-    // head node without the data
-    struct node *head = (struct node *)malloc(sizeof(struct node));
-    struct node *origin_head = head;
-    
-    // add a node next to the head node
-    head->next = (struct node *)malloc(sizeof(struct node));
-    
-    // move head to the next node
-    head = head->next;
-    
-    // set node's data
-    head->data = 1;
-    
-    // set the last node's next to NULL (meaning this node is the end of the list)
-    head->next = NULL;
-    
-    head = origin_head;
-    
-    // free
     while (head != NULL) {
-        free(head);
+        printf("%d -> ", head->data);
         head = head->next;
     }
+    printf("NULL\n");
+}
+
+int main(int argc, char **argv)
+{
+    node_t *head = (node_t *)malloc(sizeof(node_t));
+    node_t *current = head;    
+
+    current->next = (node_t *)malloc(sizeof(node_t));
+    current = current->next;
+    current->data = 1;
+    
+    current->next = (node_t *)malloc(sizeof(node_t));
+    current = current->next;
+    current->data = 3;
+  
+    current->next = (node_t *)malloc(sizeof(node_t));
+    current = current->next;
+    current->data = 5;
+    
+    current->next = NULL;
+    
+    printf("Before insert: ");
+    print_list(head);
+    
+    // create node
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+
+    // set data
+    new_node->data = 2;
+
+    // set next
+    new_node->next = (head->next)->next;
+    
+    // set pre node
+    (head->next )->next = new_node;
+
+    printf("After insert: ");
+    print_list(head);
 
     return 0;
 }
